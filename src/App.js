a61f435267a2cd6756aedb3c7e0da7ede6bbbced
Ocaxtar <ocaxtar@axtar.es>
2019-01-24 22:35:03 +0100

    Primera subida

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import {todos} from  './todos.json'
 import './App.css';
 
+
 class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+        todos
+        }
+    }
   render() {
+    const todos = this.state.todos.map((todo, i) =>{
+        return (
+            <div className="col-md-4">
+            <div className="card mt-4">
+                <div className="card-header">
+                    <h3>{todo.title}</h3>
+                    <span className="badge badge-pill badge-danger ml-2">
+                        {todo.priority}
+                    </span>
+                </div>
+                <div className="card-body">
+                    <p>{todo.description}   </p>
+                    <p><mark>{todo.responsible}</mark></p>
+                </div>
+            </div>
+            </div>
+        )
+    });
     return (
       <div className="App">
-        <header className="App-header">
+
+          <nav className="navbar navbar-dark bg-dark">
+            Tasks
+              <span className="badge badge-pill badge-light ml-2">
+                  {this.state.todos.length}
+              </span>
+            <a href="" className="text-white">
+
+            </a>
+          </nav>
+
+          <div className="container">
+              <div className="row mt-4">
+                  {todos}
+              </div>
+
+
+          </div>
+
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+
       </div>
     );
   }
